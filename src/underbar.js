4f67b0012a0fc773d2145e8934aefb6ea6aedec4
Kony Pham <kpham2@alumni.nd.edu>
2018-05-12 11:27:54 -0500

    "Filled in test value for _.uniq() test; defined functions: _.filter(), _.reject(), _.uniq();"

--- src/underbar.js
+++ src/underbar.js
@@ -85,16 +85,36 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function (item) {
+      if (test(item)) {
+        result.push (item);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var rejectTest = function (item){
+      return !test(item);
+    }
+
+    return _.filter(collection, rejectTest) ;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var set = new Set ();
+    _.each(array, function (item) {
+      set.add (item);
+    });
+
+    return [...set];
   };
 
 
