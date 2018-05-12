cb28b87cf812d2e56c010e2ccc750bf9cb15676d
Kony Pham <kpham2@alumni.nd.edu>
2018-05-12 11:35:41 -0500

    "Defined functions: _.map(), _.reduce()"

--- src/underbar.js
+++ src/underbar.js
@@ -123,6 +123,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+
+    _.each(collection, function (item){
+      result.push(iterator(item));
+    });
+
+    return result;
   };
 
   /*
@@ -164,7 +171,19 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    
+    if (arguments.length < 3){
+      accumulator = collection[0];
+      collection = collection.slice(1);
+    }
+
+    _.each(collection, function (item){
+      accumulator = iterator (accumulator, item);
+    });
+
+    return accumulator;
   };
+
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function(collection, target) {
